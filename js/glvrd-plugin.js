65874d795ce716b705dcf72bfd101608e33dcc89
Nikolay Lopin <lopinopulos@gmail.com>
2015-08-20 14:13:42 +0500

    Add autocheck

--- js/glvrd-plugin.js
+++ js/glvrd-plugin.js
@@ -5,6 +5,9 @@
 		activeHintCls: 'glvrd-underline-active',
 		hoverHintCls: 'glvrd-underline-hover',
 
+		isFirstLaunch: true,
+		delayedProofread: undefined,
+
         /**
          * Initializes the plugin, this will be executed after the plugin has been created.
          * This call is done before the editor instance has finished it's initialization so use the onInit event
@@ -23,7 +26,6 @@
             });
 
             ed.on('init', function() {
-            /* loading the content.css file, why? I have no clue */
 	            if (ed.settings.content_css !== false) {
 	               ed.dom.loadCSS(url + '/../css/glvrd.css');
 	            }
@@ -40,7 +42,7 @@
 	            	jQuery('#glvrd_section .inside .rule').html(
 	            		'<h1>' + target.data('name') + '</h1>' +
 	            		'<div class="rule-description">' + target.data('description') + '</div>'
-            		);
+            		).show('fast');
 
 	            }.bind(this));
          	}.bind(this));
@@ -48,10 +50,10 @@
          	ed.on('ExecCommand', function(e) {
          		if(e.command === 'mceInsertContent') {
          			var selection = e.target.selection,
-         				bookmark = selection.getBookmark()
+         				bookmark = selection.getBookmark(2, true),
          				cleanContent = this.removeMarkup(e.target.getContent());
 
-     				e.target.setContent(cleanContent);
+     				e.target.setContent(cleanContent, ({format: "raw"}));
      				selection.moveToBookmark(bookmark);
          		}
          	}.bind(this));
@@ -61,15 +63,38 @@
          	}.bind(this));
 
             ed.addCommand('glvrdCheckContent', function(e) {
-            	var content = me.removeMarkup(this.getContent()),
-            		strippedContent = content.replace(/(<([^>]+)>)/ig, ""),
-            		bookmark = this.selection.getBookmark();
+            	var bookmark = this.selection.getBookmark(2, true);
 
-            	window.glvrd.proofread(content, function(result) {
+            	me.proofread(this, bookmark);
+
+            	if (me.isFirstLaunch) {
+            		me.isFirstLaunch = false;
+            		jQuery(this.getBody()).on('keyup paste change keypress', function(event) {
+            			if (me.delayedProofread) {
+            				console.log('clearTimeout');
+            				clearTimeout(me.delayedProofread);
+            			}
+            			console.log('setTimeout');
+            			me.delayedProofread = setTimeout(function(){
+            				console.log('ExecutingTimeout');
+            				me.proofread(this, this.selection.getBookmark(2, true));
+            			}.bind(this), 1000);
+            		}.bind(this));
+            	}
+        	});
+        },
+
+        proofread: function(editor, bookmark) {
+        	var me = this,
+        		content = me.removeMarkup(editor.getContent()),
+        		strippedContent = content.replace(/(<([^>]+)>)/ig, "");
+
+        	window.glvrd.proofread(content, function(result) {
             		if (result.status = 'ok') {
             			var $statsBlock = jQuery('#glvrd_section .stats'),
             				offset = 0;
-            			
+
+            			jQuery('#glvrd_section .rule').hide('fast').html('');
             			$statsBlock.find('.stats-score').text(result.score);
 	            		$statsBlock.find('.stats-stopwords').text(result.fragments.length);
             			$statsBlock.find('.stats-sentences').text(me.countSentences(strippedContent));
@@ -79,7 +104,7 @@
             			if (result.fragments.length) {
             				$statsBlock.find('a.send-to-glvrd').attr('href', result.fragments[0].url);
             			}
-            			$statsBlock.show();
+            			$statsBlock.show('slow');
 
             			result.fragments.forEach(function(fragment) {
             				var tagOpen = '<span class="glvrd-underline" data-glvrd="true" data-description="' + fragment.hint.description + '" data-name="' + fragment.hint.name + '" >',
@@ -91,19 +116,20 @@
                     			+ tagClose + content.substring(fragment.end + offset, content.length);
                 			offset += tagsLength;
             			});
-            			this.setContent(content);
+            			this.setContent(content, ({format: "raw"}));
+            			this.selection.moveToBookmark(bookmark);
             		} else {
             			alert(result.message);
             		}
-            	}.bind(this));
-
-            	this.selection.moveToBookmark(bookmark);
-        	});
-        },
+            	}.bind(editor));
+		},
 
         countSentences: function(text) {
-        	var sentencesQuantity = text.match(/[^\s](\.|…|\!|\?)(?!\w)(?!\.\.)/g).length;
+        	if (text.length === 0) {
+        		return 0;
+        	}
 
+        	var sentencesQuantity = text.match(/[^\s](\.|…|\!|\?)(?!\w)(?!\.\.)/g).length;
 			if (!/(\.|…|\!|\?)/g.test(text.slice(-1))) {
   				sentencesQuantity++;
 			}
@@ -111,16 +137,23 @@
         },
 
         countWords: function(text) {
+        	if (text.length === 0) {
+        		return 0;
+        	}
+
     		return text.replace(/[А-Яа-яA-Za-z0-9-]+([^А-Яа-яA-Za-z0-9-]+)?/g, ".").length;
         },
 
         countChars: function(text) {
+        	if (text.length === 0) {
+        		return 0;
+        	}
+
     		return text.replace(/[^А-Яа-яA-Za-z0-9-\s.,()-]+/g, "").length;
         },
 
         removeMarkup: function(text) {
         	var reg = /(<span[^>]*data-glvrd="true"[^>]*>)(.+?)(<\/span>)/g;
-
         	return text.replace(reg,'$2');
         },
  
@@ -135,7 +168,7 @@
                 longname : 'Glvrd for TinyMCE',
                 author : 'Nick Lopin',
                 authorurl : 'http://lopinopulos.ru',
-                // infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/example',
+                infourl : 'http://glvrd.ru',
                 version : "0.1"
             };
         }
